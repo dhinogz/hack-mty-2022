661f010508a68a1a0d58a48dedc41eb3ec4898ad
dhinogz <davidhinojosagzz@gmail.com>
2022-09-24 11:48:21 -0500

    Set up backend

---
+++ backend/app/api/system/views.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from api.auth.token import oauth2_scheme
+
+router = APIRouter()
+
+
+@router.get("/health/")
+# async def health(token: str = Depends(oauth2_scheme)) -> None:
+async def health() -> None:
+    """
+    Checks the health of a project.
+
+    It returns 200 if the project is healthy.
+    """
+    
