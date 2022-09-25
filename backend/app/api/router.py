66da40083e51990a1bcaae0247662f29d3f6f0b2
dhinogz <davidhinojosagzz@gmail.com>
2022-09-25 07:25:24 -0500

    Auth and balance modules

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from api.system.views import router as system_router
-from api.auth.views import router as auth_router
-
+from api.balance.views import router as balance_router
+from api.user.views import router as user_router
 
 api_router = APIRouter()
+
 api_router.include_router(system_router, prefix="/system", tags=["system"])
-api_router.include_router(auth_router, prefix="", tags=["auth"])
+api_router.include_router(user_router, prefix="/user", tags=["user"])
+api_router.include_router(balance_router, prefix="/balance", tags=["balance"])
+
